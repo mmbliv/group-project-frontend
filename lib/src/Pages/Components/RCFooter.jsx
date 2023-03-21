0211a74702cfde6a8bf2916da6de008c4803d79b
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-03-21 17:59:48 -0600

    like button wire

--- lib/src/Pages/Components/RCFooter.jsx
+++ lib/src/Pages/Components/RCFooter.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './RCFooter.css'
+import LikeButton from './LikeButton'
 
 export default function RCFooter() {
     return ( 
         <div className="RCF--container">
-            <p>RCF test</p>
+            <LikeButton />
         </div>
      );
 }
