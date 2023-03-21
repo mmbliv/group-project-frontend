324456b7e2330db0c6ff7a3fff9950799920be47
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-03-21 16:32:10 -0600

    setup for react router

---
+++ lib/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.css'
+
+export default function Home() {
+    return ( 
+        <div className="home--container">
+            <p>Home Test</p>
+        </div>
+     );
+}
