25a000ab0cf5923bab481e3f8e780bdfa2af468d
locb65 <locb65@gmail.com>
2023-03-22 16:19:46 -0400

    added state to change like button color to red when clicked

--- lib/src/Pages/Components/LikeButton.jsx
+++ lib/src/Pages/Components/LikeButton.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState }from 'react';
 import './LikeButton.css'
 import {  HiOutlineHeart, HiHeart } from "react-icons/hi2"
 
 export default function LikeButton() {
+    const [liked, setLiked] = useState(false);
+
+    const handleLikeButton = () => {
+        setLiked(!liked);
+    }
     return ( 
-        <div className="like-btn--container">
+        <div className="like-btn--container" 
+        style={{backgroundColor: liked?'red':'white'}}
+        onClick={handleLikeButton}>
             <HiOutlineHeart />
         </div>
      );
