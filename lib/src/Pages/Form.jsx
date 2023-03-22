a1f5c9babe4d059333b7dfccc1163aa09b666c04
min <mmbliv@gmail.com>
2023-03-22 15:52:10 -0500

    added bodyData state and InstructionItemNumber state

--- lib/src/Pages/Form.jsx
+++ lib/src/Pages/Form.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GrAdd } from "react-icons/gr";
 import "./Form.css";
@@ -7,31 +6,84 @@ import "./Form.css";
 export default function Form() {
   const params = useParams();
   const [instructionItemNumber, setIntructionItemNumber] = useState(0);
+  const [bodyData, setBodyData] = useState({});
 
-  //   console.log(params);
+  //   const body = {};
+  function handleChange(e) {
+    if (e.target.name === "name") {
+      setBodyData((preData) => {
+        return { ...preData, name: e.target.value };
+      });
+    }
+    if (e.target.name === "description") {
+      console.log(e.target.name);
+      setBodyData((preData) => {
+        return { ...preData, description: e.target.value };
+      });
+    }
+    if (e.target.name === "ingredients") {
+      setBodyData((preData) => {
+        return { ...preData, components: e.target.value.split(",") };
+      });
+    }
+    if (e.target.name === "instrunction") {
+      setBodyData((preData) => {
+        return {
+          ...preData,
+          instruction: [
+            ...preData.instruction,
+            { position: e.target.id, display_text: e.target.value },
+          ],
+        };
+      });
+    }
+    if (e.target.name === "cook_time") {
+      setBodyData((preData) => {
+        return { ...preData, cook_time_minutes: +e.target.value };
+      });
+    }
+  }
 
+  function handleSubmit() {
+    const reqOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(bodyData),
+    };
+    fetch("http://localhost:4000/recipes", reqOptions)
+      .then((res) => res.json())
+      .then((d) => console.log(d));
+  }
   return (
     <div className="form--container">
       <form onSubmit={(e) => e.preventDefault()} className="form--content">
         <label htmlFor="" className="form--label">
           Recipe Name:
-          <input type="text" name="name" className="form--input" />
+          <input
+            type="text"
+            name="name"
+            className="form--input"
+            onChange={(e) => handleChange(e)}
+          />
         </label>
+
         <label htmlFor="" className="form--label">
           Description:
-          <input type="text" name="description" className="form--input" />
+          <input
+            type="text"
+            name="description"
+            className="form--input"
+            onChange={(e) => handleChange(e)}
+          />
         </label>
+
         <label htmlFor="" className="form--label">
           Ingredients:
           <input type="text" name="ingredients" className="form--input" />
         </label>
+
         <label htmlFor="" className="form--label">
           Instruction:
-          {/* <button
-            onClick={() => setIntructionItemNumber(instructionItemNumber + 1)}
-          >
-            Add Instruction
-          </button> */}
           <div className="form--instructions__container">
             {Array(instructionItemNumber)
               .fill(null)
@@ -40,8 +92,10 @@ export default function Form() {
                   <input
                     type="text"
                     key={i}
-                    name={`instrunction-${i + 1}`}
+                    id={i + 1}
+                    name="instrunction"
                     className="form--input"
+                    onChange={(e) => handleChange(e)}
                   />
                 );
               })}
@@ -54,11 +108,32 @@ export default function Form() {
             </button>
           </div>
         </label>
+
+        <label htmlFor="" className="form--label">
+          cook_time_minutes:
+          <input
+            type="text"
+            name="cook_time"
+            className="form--input"
+            onChange={(e) => handleChange(e)}
+          />
+        </label>
+
         <label htmlFor="" className="form--label">
           Image:
-          <input type="file" name="image" className="form--input" />
+          <input
+            type="file"
+            name="image"
+            className="form--input"
+            onChange={(e) => handleChange(e)}
+          />
         </label>
-        <button type="submit" className="form--btn form--btn__submit">
+
+        <button
+          type="submit"
+          className="form--btn form--btn__submit"
+          onClick={handleSubmit}
+        >
           Submit
         </button>
       </form>
