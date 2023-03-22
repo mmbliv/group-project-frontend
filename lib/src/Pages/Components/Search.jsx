5628663e69ac88bfd7ed966a885d352590439995
locb65 <locb65@gmail.com>
2023-03-22 16:46:49 -0400

    added mock state for search bar

--- lib/src/Pages/Components/Search.jsx
+++ lib/src/Pages/Components/Search.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState }from 'react';
 import './Search.css'
 import { BiSearch } from "react-icons/bi"
 
 export default function Search() {
+const [search, setSearch] = useState('')
+
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(search)
+}
+
+const handleSearch = (e) => {
+    setSearch(e.target.value)
+}
+
     return ( 
-        <div className="search--container">
+        // should we change the div for the search bar to <form>?
+        // will need to find a way to get this to pull a "search" from mongoDB and load that data.
+
+        <div className="search--container" onSubmit={handleSubmit}>
             <div className="search--icon">
                 <BiSearch />
             </div>
-            <input type="search" placeholder="Search..." id="search--input"/>
+            <input type="search" value={search} onChange={handleSearch} placeholder="Search..." id="search--input"/>
         </div>
      );
 }
