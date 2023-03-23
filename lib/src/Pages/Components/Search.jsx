aef6426fbce8fc232e80bf82b90a74c9b809f69f
locb65 <locb65@gmail.com>
2023-03-22 21:36:35 -0400

    have key press working for search input and state is stored

--- lib/src/Pages/Components/Search.jsx
+++ lib/src/Pages/Components/Search.jsx
@@ -6,12 +6,16 @@ export default function Search() {
 const [search, setSearch] = useState('')
 
 //uses set value to communicate with database
-
-const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(search)
+//if e.key is Enter handle the submit event and store state....
+const handleKeyPress = (e) => {
+    if(e.key === 'Enter') {
+        handleSubmit()
+    } 
 }
 
+const handleSubmit = (e) => {
+   console.log(search)
+}
 //set value
 const handleChange = (e) => {
     setSearch(e.target.value)
@@ -23,12 +27,13 @@ const handleChange = (e) => {
 
         //added temporary button to handle state changes. State is stored on submit. need to handle state change on key press of ENTER....
 
+        //notsure what to sue for in place of onKeyPress(depreciated) there are two options: onKeyUp and onKeyDown. not sure on what exactly is the difference tbh.
+
         <div className="search--container" >
             <div className="search--icon">
                 <BiSearch />
             </div>
-            <input type="search" value={search} onChange={handleChange} placeholder="Search..." id="search--input"/>
-            <button type="submit" onClick={handleSubmit}>Search</button>
+            <input type="search" value={search} onChange={handleChange} placeholder="Search..." id="search--input" onKeyDown={handleKeyPress}/>
         </div>
      );
 }
