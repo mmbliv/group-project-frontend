fba7c6849d2a957e6ed2d24d478b756abb059705
min <mmbliv@gmail.com>
2023-03-27 10:43:01 -0500

    modified some css for groceries page

--- lib/src/Pages/Groceries.jsx
+++ lib/src/Pages/Groceries.jsx
@@ -11,7 +11,12 @@ export default function Groceries() {
       {groceriesData.map((d) => {
         return <GroceriesItem key={d._id} data={d} />;
       })}
-      <button onClick={() => window.location.reload(true)}>submit</button>
+      <button
+        onClick={() => window.location.reload(true)}
+        className="groceriesItems--submit"
+      >
+        submit
+      </button>
     </div>
   );
 }
