d73aed723a7faeaf90763255b7b052e3f63cecdf
min <mmbliv@gmail.com>
2023-03-27 12:05:35 -0500

    deleted some unused code

--- lib/src/Pages/Components/GroceriesItem.jsx
+++ lib/src/Pages/Components/GroceriesItem.jsx
@@ -10,50 +10,8 @@ export default function GroceriesItem(props) {
   const { name, deleted, checked, recipe, createdAt, _id } = props.data;
   const [checkStatus, setCheckStatus] = useState(checked);
   const [deleteStatus, setDeleteStatus] = useState(deleted);
-  const checkRef = useRef();
-  const deleteRef = useRef();
 
   const date = new Date(createdAt);
-  // function handleUpdata() {
-  //   const body = { checked: checkStatus, deleted: deleteStatus };
-  //   console.log("jiji");
-  //   const reqOptions = {
-  //     method: "PUT",
-  //     headers: { "Content-Type": "application/json" },
-  //     // needed for multer package
-  //     //   enctype: "multipart/form-data",
-  //     body: JSON.stringify(body),
-  //   };
-  //   fetch(`http://localhost:4000/groceries/:${_id}`, reqOptions)
-  //     .then((res) => res.json())
-  //     .then((d) => console.log(d));
-  // }
-
-  // useEffect(() => {
-  //   window.addEventListener("beforeunload", handleUpdata);
-  // }, []);
-  // console.log(checkStatus);
-  // useBeforeUnload(
-  //   React.useCallback(() => {
-  //     console.log(checkStatus);
-  //     const body = {};
-  //     body.checked = checkStatus;
-  //     body.deleted = deleteStatus;
-  //     const reqOptions = {
-  //       method: "PUT",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify(body),
-  //     };
-  //     fetch(`http://localhost:4000/groceries/:${_id}`, reqOptions)
-  //       .then((res) => res.json())
-  //       .then((d) => console.log(d));
-  //   }, [checkStatus])
-  // );
-
-  // useBeforeUnload({
-  //   when: checkStatus !== checked,
-  //   message: "Are you sure you want to leave?",
-  // });
 
   useBeforeUnload(
     React.useCallback(() => {
