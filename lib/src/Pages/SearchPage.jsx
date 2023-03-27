6f32eb08c75ac92127ee0d42921459a4de3f83c2
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-03-27 11:21:36 -0600

    searchPage wire

--- lib/src/Pages/SearchPage.jsx
+++ lib/src/Pages/SearchPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import './SearchPage.css'
 import { Thumbnail } from './Components'
 
 export default function SearchPage() {
     return (
-        <Thumbnail />
+        <div className="searchPage--container">
+            <div className="searchPage--title__container">
+                <p className="searchPage--title">Search Results for "props"</p>
+            </div>
+            <div className="searchPage--content__container">
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+            </div>
+        </div>
      );
 }
