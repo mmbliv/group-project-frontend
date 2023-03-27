bea34089799136987475f44d8608eade3b46407f
min <mmbliv@gmail.com>
2023-03-27 09:48:18 -0500

    try custom hook to fis updata before load

--- lib/src/Pages/Components/GroceriesItem.jsx
+++ lib/src/Pages/Components/GroceriesItem.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useBeforeUnload } from "react-router-dom";
 import "./GroceriesItem.css";
 import { GiCheckMark } from "react-icons/gi";
@@ -9,28 +9,62 @@ export default function GroceriesItem(props) {
   const { name, deleted, checked, recipe, createdAt, _id } = props.data;
   const [checkStatus, setCheckStatus] = useState(checked);
   const [deleteStatus, setDeleteStatus] = useState(deleted);
+  const checkRef = useRef();
+  const deleteRef = useRef();
 
-  //   console.log(deleteStatus);
   const date = new Date(createdAt);
-  function handleUpdata() {
-    const body = { checked: checkStatus, deleted: deleteStatus };
-    console.log("jiji");
-    const reqOptions = {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      // needed for multer package
-      //   enctype: "multipart/form-data",
-      body: JSON.stringify(body),
-    };
-    fetch(`http://localhost:4000/groceries/:${_id}`, reqOptions)
-      .then((res) => res.json())
-      .then((d) => console.log(d));
-  }
+  // function handleUpdata() {
+  //   const body = { checked: checkStatus, deleted: deleteStatus };
+  //   console.log("jiji");
+  //   const reqOptions = {
+  //     method: "PUT",
+  //     headers: { "Content-Type": "application/json" },
+  //     // needed for multer package
+  //     //   enctype: "multipart/form-data",
+  //     body: JSON.stringify(body),
+  //   };
+  //   fetch(`http://localhost:4000/groceries/:${_id}`, reqOptions)
+  //     .then((res) => res.json())
+  //     .then((d) => console.log(d));
+  // }
 
   // useEffect(() => {
   //   window.addEventListener("beforeunload", handleUpdata);
   // }, []);
-  useBeforeUnload(handleUpdata());
+  // console.log(checkStatus);
+  // useBeforeUnload(
+  //   React.useCallback(() => {
+  //     console.log(checkStatus);
+  //     const body = {};
+  //     body.checked = checkStatus;
+  //     body.deleted = deleteStatus;
+  //     const reqOptions = {
+  //       method: "PUT",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify(body),
+  //     };
+  //     fetch(`http://localhost:4000/groceries/:${_id}`, reqOptions)
+  //       .then((res) => res.json())
+  //       .then((d) => console.log(d));
+  //   }, [checkStatus])
+  // );
+
+  useBeforeUnload(
+    React.useCallback(() => {
+      console.log(checkStatus);
+      const body = {};
+      body.checked = checkStatus;
+      body.deleted = deleteStatus;
+      const reqOptions = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      };
+      fetch(`http://localhost:4000/groceries/${_id}`, reqOptions)
+        .then((res) => res.json())
+        .then((d) => console.log(d));
+    }, [checkStatus, deleteStatus, _id])
+  );
 
   if (!deleted && !deleteStatus)
     return (
--- lib/src/Pages/Groceries.jsx
+++ lib/src/Pages/Groceries.jsx
@@ -1,10 +1,26 @@
 import GroceriesItem from "./Components/GroceriesItem";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useBeforeUnload } from "react-router-dom";
 import "./Groceries.css";
 import React from "react";
 
 export default function Groceries() {
   const groceriesData = useLoaderData();
+  useBeforeUnload(
+    React.useCallback(() => {
+      console.log("huhu");
+      // const body = {};
+      // body.checked = checkStatus;
+      // body.deleted = deleteStatus;
+      // const reqOptions = {
+      //   method: "PUT",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify(body),
+      // };
+      // fetch(`http://localhost:4000/groceries/${_id}`, reqOptions)
+      //   .then((res) => res.json())
+      //   .then((d) => console.log(d));
+    }, [])
+  );
   //   console.log(groceriesData);
   return (
     <div className="groceriesItems--container">
