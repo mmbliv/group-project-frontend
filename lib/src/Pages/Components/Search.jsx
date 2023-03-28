038ee9f2135f7e09e96ac4804b6146281601f2ab
Eric_Spychalski <EricSpychalski@gmail.COM>
2023-03-28 14:56:09 -0600

    passing search results data to SearchPage

--- lib/src/Pages/Components/Search.jsx
+++ lib/src/Pages/Components/Search.jsx
@@ -1,36 +1,32 @@
 import React, { useState }from 'react';
 import './Search.css'
 import { BiSearch } from "react-icons/bi"
-import { useNavigate, withRouter } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import axios from "axios"
 
 export default function Search({setSearchResults}) {
 const [search, setSearch] = useState('')
 const navigate = useNavigate()
 
-
 const handleKeyPress = (e) => {
     if(e.key === 'Enter') {
         handleSubmit()
-        navigate('/search')
     } 
 }
 
 const handleSubmit = async(e) => {
     const res = await axios.get(`http://localhost:4000/recipes/name/${search}`)
-    setSearchResults(res.data)
+    setSearch(res.data)
     console.log(res.data)
     console.log(search)
+    //passes data from @here to '/search'. state picked up by useLocation on SearchPage
+    navigate('/search', {state: {results: res.data}})
 }
+
 //set value
 const handleChange = (e) => {
     setSearch(e.target.value)
-
 }
-
-// const nextPath = (path) => {
-//     this.history.push(path)
-// }
 
     return ( 
         //notsure what to sue for in place of onKeyPress(depreciated) there are two options: onKeyUp and onKeyDown. not sure on what exactly is the difference tbh.
--- lib/src/Pages/SearchPage.jsx
+++ lib/src/Pages/SearchPage.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import './SearchPage.css'
 import { Thumbnail } from './Components'
+import { useLocation } from 'react-router-dom'
 import Search from './Components/Search.jsx';
 
 export default function SearchPage() {
-    const [searchResults, setSearchResults] = useState([]);
+    // const [searchResults, setSearchResults] = useState([]);
+    const { state}  = useLocation()
+    const { results } = state;
+    const searchResults = []
+    console.log(results)
 
-   const handleSearchResults = (results)=> {
-        setSearchResults(results);
-        console.log(results)
-    }
+//    const handleSearchResults = (results)=> {
+//         setSearchResults(results);
+//         console.log(results)
+//     }
+
+    results.map(({ _id, name, img }) => {
+        if(img !== null){
+            return(
+                searchResults.push(
+                    <Thumbnail name={name} img={img} key={_id}/>
+                )
+            )
+        }
+    })
+
+    
 
     return (
         <div className="searchPage--container">
@@ -17,7 +34,7 @@ export default function SearchPage() {
                 <p className="searchPage--title">Search Results for "props"</p>
             </div>
             <div className="searchPage--content__container">
-                {/* <Thumbnail searchResults={searchResults}/> */}
+                {searchResults}
             </div>
         </div>
      );
